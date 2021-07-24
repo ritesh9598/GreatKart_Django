db949980d5513f6fdbfcb506395e2c9001a683bf
ritesh98 <riteshraja10436@gmail.com>
2021-07-24 13:00:08 +0530

    store added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signin(request):
+    return render(request,'accounts/signin.html')
+def register(request):
+    return render(request,'accounts/register.html')
